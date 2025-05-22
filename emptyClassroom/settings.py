394e36ff473b32f338727d47e197e890506d8bd8
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2025-05-21 21:19:36 -0400

    refactor email configuration to use GMAIL_EMAIL environment variable (typo)

--- emptyClassroom/settings.py
+++ emptyClassroom/settings.py
@@ -142,14 +142,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
+EMAIL_HOST_USER = os.environ.get('GMAIL_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_USER')
+DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_EMAIL')
 
 
 # Django Email Verification
 DJANGO_EMAIL_VERIFICATION = {
-    'EMAIL_FROM_ADDRESS': os.environ.get('GMAIL_USER'),
+    'EMAIL_FROM_ADDRESS': os.environ.get('GMAIL_EMAIL'),
     'EMAIL_MAIL_SUBJECT': 'Confirm your email',
     'EMAIL_MAIL_HTML': 'verification_email.html',
     'EMAIL_MAIL_PLAIN': 'verification_email.txt',
